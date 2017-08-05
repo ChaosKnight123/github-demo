dd28982f949201176dfb9891ecca8d75b5b51edc
ChaosKnight123 <dovancuongchelsea@gmail.com>
2017-08-05 07:46:48 -0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
  
     <body>
  
-        <a href="https://www.facebook.com/dialog/oauth?client_id=509011619175617&redirect_uri=http://localhost/fblogin/callback.php&scope=public_profile"><img src="facebook-sign-in.png"/></a>
+        <a href="https://www.facebook.com/dialog/oauth?client_id=114791355846445&redirect_uri=chaosknight123.github.io/news&scope=public_profile"><img src="facebook-sign-in.png"/></a>
  
     </body>
  
